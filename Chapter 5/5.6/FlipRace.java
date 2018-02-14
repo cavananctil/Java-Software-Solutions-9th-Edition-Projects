98e8c9ac463ffc91a16a8411eb48c0ab8a918d0a
cavananctil <36462237+cavananctil@users.noreply.github.com>
2018-02-13 17:08:26 -0800

    Update FlipRace.java

--- Chapter 5/5.6/FlipRace.java
+++ Chapter 5/5.6/FlipRace.java
@@ -7,17 +7,16 @@ public class FlipRace {
             coin1.flip(); coin2.flip();            
             count++;
             if (coin1.isHeads()==true){heads1++;}
+            else {heads1=0}
             if (coin2.isHeads()==true){heads2++;}
+            else {heads2=0}
             System.out.println("Round :"+count);
             System.out.println("Coin1: "+coin1);
             System.out.println("Coin2: "+coin2);
         }
-        if (heads1==3)
-            System.out.println("Coin1 wins!");
-        else if (heads2==3)
-            System.out.println("Coin2 wins!");
-        else
-            System.out.println("Its a tie!");
-        System.out.println("Flips: "+count);
+               if(heads2 == heads1) {System.out.println("Its a tie!");}
+         else 
+           {if (heads1 ==3) {System.out.println("Coin1 wins!");}
+            else {System.out.println("Coin2 wins!");}}
     }
 }
